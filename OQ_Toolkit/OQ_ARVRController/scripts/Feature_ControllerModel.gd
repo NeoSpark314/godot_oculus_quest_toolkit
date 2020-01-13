881135e9164fba3ae64acebfe779ef0fe13e4c9a
Holger Dammertz <holger.dammertz@googlemail.com>
2020-01-13 20:00:12 +0100

    add also a grip marker to controllers to have a consistent orientation point to attach to

--- OQ_Toolkit/OQ_ARVRController/scripts/Feature_ControllerModel.gd
+++ OQ_Toolkit/OQ_ARVRController/scripts/Feature_ControllerModel.gd
@@ -2,3 +2,4 @@ extends Spatial
 
 
 onready var palm_marker = $PalmMarker;
+onready var grab_marker = $GrabMarker;
--- OQ_Toolkit/OQ_ARVRController/scripts/Feature_HandModel.gd
+++ OQ_Toolkit/OQ_ARVRController/scripts/Feature_HandModel.gd
@@ -4,6 +4,7 @@
 extends Spatial
 
 onready var palm_marker = $PalmMarker;
+onready var grab_marker = $GrabMarker;
 
 var hand : ARVRController = null;
 var model : Spatial = null;
