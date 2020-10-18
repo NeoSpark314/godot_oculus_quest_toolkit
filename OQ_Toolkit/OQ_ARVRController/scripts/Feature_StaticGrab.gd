e2c88dff1ede7974568c07de753c9eb4b7ea35d4
Adam Brown <adamwbrown@gmail.com>
2020-10-18 07:18:56 -0700

    Made StaticGrab configurable & work with hand tracking (#39)
    * Made StaticGrab configurable & work with hand tracking
    
    - StaticGrab now can use a hand tracking gesture instead of controller button
    - StaticGrab now has an export var for configuring which physics layer should be used for detecting objects to grab
    
    * Added hand tracking for RigidBodyGrab
    
    - Fixed warning spam
    
    Don't allow hiding hand tracking models
    
    If they are hidden, gesture detection stops updating

--- OQ_Toolkit/OQ_ARVRController/scripts/Feature_StaticGrab.gd
+++ OQ_Toolkit/OQ_ARVRController/scripts/Feature_StaticGrab.gd
@@ -10,27 +10,57 @@ export var check_parent_can_static_grab = false;
 signal oq_static_grab_started;
 signal oq_static_grab_ended;
 
+export(vr.CONTROLLER_BUTTON) var grab_button = vr.CONTROLLER_BUTTON.GRIP_TRIGGER;
+export(String) var grab_gesture := "Fist"
+export(int, LAYERS_3D_PHYSICS) var grab_layer := 1
+
 var is_grabbing = false;
 var is_just_grabbing = false;
 var grabbed_object = null;
 var grab_position = Vector3();
 var delta_position = Vector3();
+var last_gesture := "";
 
 var _additional_grab_checker = null;
 
-export(vr.CONTROLLER_BUTTON) var grab_button = vr.CONTROLLER_BUTTON.GRIP_TRIGGER;
 
 func _ready():
 	controller = get_parent();
 	if (not controller is ARVRController):
 		vr.log_error(" in Feature_StaticGrab: parent not ARVRController.");
 	grab_area = $GrabArea;
+	grab_area.collision_mask = grab_layer;
+
+
+func just_grabbed() -> bool:
+	var did_grab: bool
 	
+	if controller.is_hand:
+		var cur_gesture = controller.get_hand_model().detect_simple_gesture()
+		did_grab = cur_gesture != last_gesture and cur_gesture == grab_gesture
+		last_gesture = cur_gesture
+	else:
+		did_grab = controller._button_just_pressed(grab_button)
+	
+	return did_grab
+
+
+func not_grabbing() -> bool:
+	var not_grabbed: bool
+	
+	if controller.is_hand:
+		last_gesture = controller.get_hand_model().detect_simple_gesture()
+		not_grabbed = last_gesture != grab_gesture
+	else:
+		not_grabbed = !controller._button_pressed(grab_button)
+	
+	return not_grabbed
+
 	
 func _physics_process(_dt):
 	grab_area.global_transform = controller.get_palm_transform();
-	if (controller._button_just_pressed(grab_button)):
 
+	if (just_grabbed()):
 		if (_additional_grab_checker):
 			grabbed_object = _additional_grab_checker.oq_additional_static_grab_check(grab_area, controller);
 
@@ -55,7 +85,7 @@ func _physics_process(_dt):
 	else:
 		is_just_grabbing = false;
 	
-	if (!controller._button_pressed(grab_button)):
+	if (not_grabbing()):
 		if (is_grabbing):
 			emit_signal("oq_static_grab_ended", grabbed_object, controller)
 			grabbed_object = null;
