eced9557dd84c5cec130d820c92d962b1b391d29
neoxid501 <carlo97dm@gmail.com>
2021-02-04 16:20:50 -0800

    Move the Origin for the Trigger

--- OQ_Toolkit/OQ_ARVRController/scripts/Feature_ControllerModel_Left_Reactive.gd
+++ OQ_Toolkit/OQ_ARVRController/scripts/Feature_ControllerModel_Left_Reactive.gd
@@ -1,10 +1,15 @@
 extends Spatial
 
+#constants
+MENU_PRESSED = -.001
+X_PRESSED = -.002
+Y_PRESSED = -.002
 
 func _process(_delta):
 	update_buttons();
 
 # updates the positions of the buttons based on what is currently pressed
 #  ideally the digital buttons would be updated on a signal rather than every delta
-#  but in the interest of not modifying current architecture it is done this way
+#  but in the interest of not modifying existing architecture it is done this way
 func update_buttons():
+	pass
